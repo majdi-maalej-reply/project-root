126704ea894ef93410228c42fffc1e311716cb89
majdi-maalej-reply <m.maalej@reply.de>
2026-02-02 14:07:53 +0100

    Create providers.tf

---
+++ terraform/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  
+  backend "azurerm" {
+    # Configure after creating storage account
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
