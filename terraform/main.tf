7f65a14b32026c3d2a4eb4e6de4a6f0b2e49afcd
majdi-maalej-reply <m.maalej@reply.de>
2026-02-02 14:47:49 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,3 +32,10 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
